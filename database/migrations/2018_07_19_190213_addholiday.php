e9e4c06672fa0269d8316557d763f065812250d2
cckaron <cg.workst@gmail.com>
2018-07-20 21:06:31 +0800

    fourth

---
+++ database/migrations/2018_07_19_190213_addholiday.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Addholiday extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
